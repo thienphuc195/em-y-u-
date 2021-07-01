6c128f089a094cce4eb33a0fba09115da3d72457
thienphuc195 <86764664+thienphuc195@users.noreply.github.com>
2021-07-01 13:18:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,7 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Babe à <3',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    Mà sao em lạnh lùng với anh vậy T.T`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
